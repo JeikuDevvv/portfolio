f6242cd59f1c9759c2f44953b14c59b6d6161331
Jeiku Dev <shippai.jeikudev@gmail.com>
2022-12-07 12:47:07 +0800

    monkey me

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const testFunction = () => {
-  console.log('Testing function');
+const facebookButton = () => {
+  location.href = 'https://www.facebook.com/lourenceeeee/';
+};
+
+const twitterButton = () => {
+  location.href = 'https://twitter.com/lourence_jake';
+};
+
+const linkedinButton = () => {
+  location.href = 'https://www.linkedin.com/in/jake-lourence-villar-b00723246/';
+};
+
+const aboutButton = () => {
+  location.href =
+    'https://media.tenor.com/I0n7w-UIzycAAAAC/arangutan-monkey.gif';
 };
